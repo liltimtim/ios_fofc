d53bc7d11d5a3ff3b217da9a70b68a42a6f3507b
Timothy Dillman <timothy.dillman@mobilefirst.me>
2022-06-28 23:01:06 -0500

    Initial Commit

---
+++ FOCF/FOCFApp.swift
@@ -0,0 +1,17 @@
+//
+//  FOCFApp.swift
+//  FOCF
+//
+//  Created by Timothy Dillman on 6/28/22.
+//
+
+import SwiftUI
+
+@main
+struct FOCFApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
